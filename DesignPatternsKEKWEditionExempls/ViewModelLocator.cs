58b3fc428558a517ff47a822892911f9f46191c0
saldatCrus <dimakoval0198@gmail.com>
2021-03-30 16:24:40 +0300

    Добавьте файлы проекта.

---
+++ DesignPatternsKEKWEditionExempls/ViewModelLocator.cs
@@ -0,0 +1,40 @@
+﻿using DesignPatternsKEKWEditionExempls.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsKEKWEditionExempls
+{
+    class ViewModelLocator
+    {
+        private static ServiceProvider _provaider;
+
+
+        public static void Init()
+        {
+            var services = new ServiceCollection();
+
+            //My ViewModels
+            services.AddTransient<MainViewModel>();
+
+
+            // My service
+            //services.AddSingleton<NavigationService>();
+            //services.AddSingleton<EventBus>();
+            //services.AddSingleton<MessageBus>();
+
+            _provaider = services.BuildServiceProvider();
+
+            foreach (var item in services)
+            {
+                _provaider.GetRequiredService(item.ServiceType);
+            }
+        }
+
+        public MainViewModel MainViewModel => _provaider.GetRequiredService<MainViewModel>();
+
+    }
+}
---
+++ DesignPatternsKEKWEditionExempls/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsKEKWEditionExempls.ViewModels
+{
+    class MainViewModel : BindableBase
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
